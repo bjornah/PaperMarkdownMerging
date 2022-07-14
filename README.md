965e897e0f78467325462a668bd02d12160c6d3b
bjornah <bjorn.victor.ahlgren@gmail.com>
2022-07-14 18:31:57 +0200

    fixed bugs and cleaned up

---
+++ README.md
@@ -0,0 +1,26 @@
+# README
+
+### Small program to help organise scientific papers in a simple, portable way
+
+Creates master document for paper summaries. Requires a directory structure where the master document ends up in the base folder. This base folder requires two subfolders, pdfs/ and summaries/. pdfs/ holds the pdfs associated with each paper and these will be linked to in the master document via hyper refs. summaries/ holds markdown documents summarising each paper. These files should not contain hyper links. It is a good idea to in these files give the full title of the paper. The naming convention for all .md and .pdf files should be "Firstauthor+year" + file ending. In the case of multiple papers ending up with the same name, add Secondauthor, etc. before "+year".
+
+If you want to set a default path, add file "default_path.txt" to ./credentials, containing
+
+    [defaults]
+    default_path=your_default_path
+### Outline of the basedir
+
+- basedir/
+    - pdfs/
+      - Firstauthor+year().pdf
+    - summaries/
+      - Firstauthor+year().md
+    - paper_summaries_master.md
+
+where each paper summary document should start with 
+    
+    ### FirstAuthor et al. (year)
+    ##### Full paper title.
+
+In case the .md file is missing for a paper where the pdf is present, an .md file will be created, containing only "### FirstAuthor et al. (year)"
+
---
+++ src/PaperMarkdownMerging/merge.py
@@ -0,0 +1,96 @@
+import os
+from glob import glob
+from natsort import natsorted
+import fileinput
+import argparse
+import configparser
+import pathlib
+
+package_base_dir = pathlib.Path(__file__).parent.parent.parent.resolve()
+
+def parse_args():
+    config = configparser.ConfigParser()
+    cred_file = os.path.join(package_base_dir,'credentials','default_path.txt')
+    if os.path.exists(cred_file):
+        config.read(cred_file)
+        default_path = config['defaults']['default_path']
+    else:
+        default_path = './'
+
+    parser = argparse.ArgumentParser(description='Creates master document for paper summaries. \
+        Requires a directory structure where the master document ends up in the base folder. \
+        This base folder requires two subfolders, pdfs/ and summaries/. pdfs/ holds the pdfs \
+        associated with each paper and these will be linked to in the master document via \
+        hyper refs. summaries/ holds markdown documents summarising each paper. These \
+        files should not contain hyper links. It is a good idea to in these files give the full \
+        title of the paper. The naming convention for all .md and .pdf files should be \
+        "Firstauthor+year" + file ending. In the case of multiple papers ending up with the \
+        same name, add Secondauthor, etc. before "+year".')
+
+    parser.add_argument(
+        '--basedir', 
+        type=str, 
+        default=default_path,
+        help='Path to base directory. Needs to hold directories "summaries" and "pdfs".'
+    )
+
+    return parser
+
+def get_all_papers(source_document_path, source_pdf_path):
+    papers_md = glob(os.path.join(source_document_path,'*md'))
+    papers_pdf = glob(os.path.join(source_pdf_path,'*pdf'))
+    paper_names = []
+    for paper in papers_md:
+        name = paper.split('\\')[-1].split('.')[0]
+        if name not in paper_names:
+            paper_names.append(name)
+    for paper in papers_pdf:
+        name = paper.split('\\')[-1].split('.')[0]
+        if name not in paper_names:
+            paper_names.append(name)
+    paper_names = natsorted(paper_names)
+    return paper_names
+
+def get_name(paper_name):
+    name = paper_name.replace('+', ' et al. (')+')'
+    return name
+
+def write_link(paper_name):
+    name = get_name(paper_name)
+    rel_path = os.path.join('pdfs', f'{paper_name}.pdf')
+    write_string = f'### [{name}]({rel_path})\n'
+    return write_string
+        
+if __name__ == '__main__':
+    parser = parse_args()
+    args = parser.parse_args()
+
+    basedir = os.path.abspath(args.basedir)
+
+    source_document_path = os.path.join(basedir, 'summaries')
+    source_pdf_path = os.path.join(basedir, 'pdfs')
+    master_document_path = os.path.join(basedir, 'paper_summaries_master.md')
+    
+    paper_names = get_all_papers(source_document_path, source_pdf_path)
+    
+    with open(master_document_path, 'w') as fo:
+        for paper_name in paper_names:
+            md_name = os.path.join(source_document_path, f'{paper_name}.md')
+            pdf_name = os.path.join(source_pdf_path, f'{paper_name}.pdf')
+            if not os.path.exists(md_name):
+                print(f'{md_name} does not exist. Will create it.')
+                with open(md_name, 'w') as foo:
+                    
+                    line = get_name(paper_name)
+                    foo.write(f'### {line}\n')
+
+            for i,line in enumerate(fileinput.input(md_name)):
+                if i==0:
+                    if not os.path.exists(pdf_name):
+                        print(f'{pdf_name} does not exist')
+                        name = get_name(paper_name)
+                        line = f'### {name} [pdf is missing]\n'
+                    else:
+                        line = write_link(paper_name)
+                fo.write(line)
+            fo.write('\n\n')
